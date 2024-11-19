7145d532f8a79b5c9e1bcfbe4af7a81ac948470f
Max Kapur <maxkapur@gmail.com>
2024-11-19 16:59:38 -0500

    Update `configure.sh` to use conda/mamba instead of rbenv
    rbenv has proven unreliable across platforms and takes a long time
    to compile Ruby from scratch. Ruby binaries available in conda-forge
    seem fairly easy to work with. Note that `configure.sh` isn't
    actually used in any of our CI (yet) but this may change as we move
    toward a self-contained build routine (and away from the chain of
    GitHub actions).

--- configure.sh
+++ configure.sh
@@ -1,18 +1,26 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-# Configure a workstation to build/develop the static site. Tested on latest
-# Ubuntu release.
+# Configure a workstation to build/develop the static site. Tested on Ubuntu
+# 22.04.
 
 set -x
-if command -v git > /dev/null
-then
-    sudo apt update -y
-    sudo apt upgrade -y
-    sudo apt install -y rbenv ruby-build
-else
-    warn "OS is not Debian-based; skipped installing rbenv and ruby-build"
-fi
-eval "$(rbenv init -)"
-rbenv install --skip-existing
-bundle install
-# bundle exec jekyll serve
+set -e
+
+TMP_CONDA_PREFIX=$(realpath "./.conda")
+
+# https://github.com/mamba-org/mamba/issues/1478
+CI="True"
+
+mkdir -p ./vendor/
+if [ -d "$TMP_CONDA_PREFIX" ]; then rm -ri $TMP_CONDA_PREFIX; fi
+mamba create \
+    --yes \
+    --prefix "$TMP_CONDA_PREFIX" \
+    --no-default-packages \
+    "conda-forge::compilers=1.8.*" \
+    "conda-forge::make=4.*" \
+    "conda-forge::ruby=3.2.*"
+# Fixes issue where bundle looks for ruby3.2 instead of ruby
+ln -s "$TMP_CONDA_PREFIX/bin/ruby" "$TMP_CONDA_PREFIX/bin/ruby3.2"
+mamba run --prefix "$TMP_CONDA_PREFIX" --no-capture-output bundle install
+# mamba run --prefix ./vendor/conda bundle exec jekyll serve
