95e3fb67bcd2bc33c88a023a0721e20e953e6fa5
Max Kapur <maxkapur@gmail.com>
2024-11-21 16:10:15 -0600

    Remove `set -x` and `set -e` options now that script is simple
    Add some comments

--- configure.sh
+++ configure.sh
@@ -3,13 +3,12 @@
 # Configure a workstation to build/develop the static site. Tested on Fedora
 # Silverblue 40 and Ubuntu 22.04 (via GitHub Actions runner) with Miniforge.
 
-set -x
-set -e
-
+# Remove existing .conda/ environment if it exists. Pipe yes to this script to
+# automatically confirm.
 TMP_CONDA_PREFIX=$(realpath "./.conda")
 if [ -d "$TMP_CONDA_PREFIX" ]; then rm -ri "$TMP_CONDA_PREFIX"; fi
 
-# CI=True to prevent weird progress bar
+# Create conda environment. Set CI=True to prevent weird progress bar:
 # https://github.com/mamba-org/mamba/issues/1478
 CI="True" mamba create \
     --yes \
@@ -22,5 +21,5 @@ CI="True" mamba create \
     "ruby=3.3.*" \
     "shellcheck=0.10.*"
 
-# Install Ruby/Bundler deps
+# Install Ruby/Bundler deps.
 mamba run --prefix "$TMP_CONDA_PREFIX" --no-capture-output bundle install
