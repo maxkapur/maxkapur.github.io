fed2bbabf30c99e8c2312d323f5e1ae1d7dada0e
Max Kapur <git@maxkapur.com>
2024-12-03 19:00:24 -0500

    Use `--json` instead of grep to check conda updated
    https://github.com/conda/conda/issues/14418#issuecomment-2513806325

--- _scripts/check_conda_updated.sh
+++ _scripts/check_conda_updated.sh
@@ -3,8 +3,6 @@
 # Check that all installed conda packages are up to date
 function check_conda_updated () {
     TEMP_FILE="$(mktemp)"
-    conda update --all --dry-run | tee "$TEMP_FILE"
-    # grep for the message that conda update prints if packages are up to date.
-    # grep exits with 0 if it is not found (meaning that updates are available).
-    grep -q "requested packages already installed" "$TEMP_FILE"
+    mamba update --all --dry-run --json | tee "$TEMP_FILE"
+    jq -e '.success and (.actions | length) == 0' "$TEMP_FILE"
 }
