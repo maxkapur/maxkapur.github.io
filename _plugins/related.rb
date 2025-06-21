9a3f5fc02831de91a4bac6f9af85284d7c9afcd0
Max Kapur <git@maxkapur.com>
2025-06-21 09:43:21 -0500

    Minimal toy example
    Evaluates "similarity" as "similar character count" (placeholder)
    behavior. Shows how to access properties in the post template.

--- _plugins/related.rb
+++ _plugins/related.rb
@@ -1,8 +1,19 @@
 module Related
   class Generator < Jekyll::Generator
     def generate(site)
-      site.posts.docs.filter { |post| !post.data["hidden"] }.each do |post|
-        post.data["related"] = ["placeholder1", "placeholder2"]
+      related_data = site.posts.docs.map do |post|
+        # Placeholder; eventually we will put count vector data here
+        [post, post.content.length]
+      end
+
+      site.posts.docs.each do |current_post|
+        similarity_scores = related_data.map do |related_post, length|
+          {
+            "post" => related_post,
+            "score" => (current_post.content.length - length).abs
+          }
+        end.sort_by { |item| item["score"] }
+        current_post.data["related"] = similarity_scores
       end
     end
   end
