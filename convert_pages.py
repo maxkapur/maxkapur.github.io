e113ce9bee847a918cd6756065716e0cc43850b4
Max Kapur <git@maxkapur.com>
2026-05-24 16:37:51 -0500

    Create aliases for pages, too

--- convert_pages.py
+++ convert_pages.py
@@ -1,3 +1,4 @@
+from datetime import date
 from pathlib import Path
 
 import saneyaml
@@ -5,13 +6,20 @@ import toml
 import tomllib
 
 
-def process(page: Path):
-    if page.name == "_index.md":
-        print(f"Skipping {page}")
+def process(path: Path):
+    if path.parts[1] == "pages":
+        page_type = "page"
+    elif path.parts[1] == "posts":
+        page_type = "post"
+    else:
+        raise ValueError
+
+    if path.name == "_index.md":
+        print(f"Skipping {path}")
         return
 
-    print(f"Processing {page}")
-    original_text = page.read_text()
+    print(f"Processing {path}")
+    original_text = path.read_text()
 
     if original_text.strip().startswith("---"):
         print("yaml frontmatter")
@@ -26,9 +34,20 @@ def process(page: Path):
         body = original_text
         d = {}
 
-    # Every page in the Jekyll site had a permalink so there's no default path
-    # to alias
-    d["aliases"] = d.get("aliases", [])
+    # Construct aliases to maintain paths from Jekyll site
+    if page_type == "page":
+        # Every page in the Jekyll site had a permalink so there's no default
+        # path to alias
+        default_aliases = []
+    elif page_type == "post":
+        date_str = path.name[:10]
+        slug = path.name[11:-3]  # strip date and ".md"
+        assert date.fromisoformat(date_str)  # check format
+        default_aliases = [f"/{date_str.replace('-', '/')}/{slug}.html"]
+    else:
+        raise ValueError
+
+    d["aliases"] = d.get("aliases", default_aliases)
     if permalink := d.get("permalink"):
         # Jekyll permalink
         d["aliases"].append(permalink)
@@ -41,10 +60,12 @@ def process(page: Path):
     body = body.strip()
     output = f"+++\n{frontmatter}+++\n\n{body}"
     if output != original_text:
-        page.write_text(output)
+        path.write_text(output)
 
 
 if __name__ == "__main__":
-    pages = list(Path("./content/pages/").glob("**.md"))
+    pages = list(Path("./content/pages/").glob("**.md")) + list(
+        Path("./content/posts/").glob("**.md")
+    )
     for page in pages:
         process(page)
