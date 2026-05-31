34080c494c1101f0bfbf2f1cccfbfdfe0598aec0
Max Kapur <git@maxkapur.com>
2026-05-31 17:44:11 -0500

    Turns out I pipx install ruff

--- configure.sh
+++ configure.sh
@@ -7,12 +7,20 @@ else
     sudo snap install hugo
 fi
 
-APT_PACKAGES=(npm python3-requests python3-ruff python3-saneyaml python3-tinycss2 python3-toml shellcheck)
+APT_PACKAGES=(npm pipx python3-requests python3-saneyaml python3-tinycss2 python3-toml shellcheck)
 if dpkg-query -l "${APT_PACKAGES[@]}" >/dev/null
 then
     echo "apt packages already installed"
 else
     sudo apt-get install "${APT_PACKAGES[@]}" --yes --quiet
+fi
+
+if command -v ruff >/dev/null
+then
+    echo "ruff already installed"
+else
+    pipx ensurepath
+    pipx install ruff
 fi
 
 if [[ -d node_modules ]]
