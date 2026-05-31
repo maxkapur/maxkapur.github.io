6e0dc0ab2fda49ec2475898905f78b9ac806dca4
Max Kapur <git@maxkapur.com>
2026-05-31 17:39:46 -0500

    Add ruff to apt dependencies

--- configure.sh
+++ configure.sh
@@ -7,7 +7,7 @@ else
     sudo snap install hugo
 fi
 
-APT_PACKAGES=(npm python3-requests python3-saneyaml python3-tinycss2 python3-toml shellcheck)
+APT_PACKAGES=(npm python3-requests python3-ruff python3-saneyaml python3-tinycss2 python3-toml shellcheck)
 if dpkg-query -l "${APT_PACKAGES[@]}" >/dev/null
 then
     echo "apt packages already installed"
