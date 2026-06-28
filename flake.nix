763ac65e99f9c171d562017a90b19e332ae04181
Max Kapur <git@maxkapur.com>
2026-06-28 09:40:01 -0500

    Add htmltest and config
    Internal links only for now

--- flake.nix
+++ flake.nix
@@ -14,6 +14,7 @@
     {
       devShells.x86_64-linux.default = pkgs.mkShell {
         buildInputs = with pkgs; [
+          htmltest
           hugo
           nixfmt
           prettier
--- lint.sh
+++ lint.sh
@@ -2,8 +2,14 @@
 
 set -ex
 
+# These checks have mutating counterparts in format.sh
 nixfmt flake.nix --check
 prettier . --check --verbose
 ruff format --check
 ruff check
 shellcheck ./*.sh
+
+# These checks don't
+TEMP_DIR="$(mktemp --directory)"
+hugo build --destination "$TEMP_DIR"
+htmltest --conf .htmltest.yml --skip-external "$TEMP_DIR"
